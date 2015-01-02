f65c864abf25447713c52f8411c8f05eb3b38e8d
jessemillman <jesse.millman@wge.com.au>
2015-01-02 11:48:02 +0800

    changed to return lists to allow for multiple shifts in the one sheet

--- jeremy-project/Print.cs
+++ jeremy-project/Print.cs
@@ -8,17 +8,21 @@ namespace jeremy_project
 {
     class Print
     {
-        public static void PrintShiftTimes(Shift shift)
-        {            
-            // print any info you need based off the object.
-            Console.WriteLine(shift.Days[0] + " = " + shift.Day1);
-            Console.WriteLine(shift.Days[1] + " = " + shift.Day2);
-            Console.WriteLine(shift.Days[2] + " = " + shift.Day3);
-            Console.WriteLine(shift.Days[3] + " = " + shift.Day4);
-            Console.WriteLine(shift.Days[4] + " = " + shift.Day5);
-            Console.WriteLine(shift.Days[5] + " = " + shift.Day6);
-            Console.WriteLine(shift.Days[6] + " = " + shift.Day7);
-            Console.WriteLine("Total Hours = " + shift.ShiftTotal);
+        public static void PrintShiftTimes(List<Shift> shifts)
+        {
+            foreach (Shift shift in shifts)
+            {
+                // print any info you need based off the object.
+                Console.WriteLine(shift.Days[0] + " = " + shift.Day1);
+                Console.WriteLine(shift.Days[1] + " = " + shift.Day2);
+                Console.WriteLine(shift.Days[2] + " = " + shift.Day3);
+                Console.WriteLine(shift.Days[3] + " = " + shift.Day4);
+                Console.WriteLine(shift.Days[4] + " = " + shift.Day5);
+                Console.WriteLine(shift.Days[5] + " = " + shift.Day6);
+                Console.WriteLine(shift.Days[6] + " = " + shift.Day7);
+                Console.WriteLine("Total Hours = " + shift.ShiftTotal);
+                Console.WriteLine(String.Empty);
+            }
         }
     }
 }
